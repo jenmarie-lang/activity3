5bd3b6639f88d6c5ddd10cee28c44c210ea04768
jenmarie-lang <143498123+jenmarie-lang@users.noreply.github.com>
2023-09-24 21:24:07 +0800

    ging
    commit to master

--- App.js
+++ App.js
@@ -1,20 +1,75 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Button, Text } from 'react-native';
+import { Ionicons } from '@expo/vector-icons'; // Import Ionicons from @expo/vector-icons
 
 export default function App() {
+  const [isFlashlightVisible, setIsFlashlightVisible] = useState(false);
+  const [isFlashlightOn, setIsFlashlightOn] = useState(false);
+  const [counterValue, setCounterValue] = useState(0);
+  const [isCounterVisible, setIsCounterVisible] = useState(false);
+
+  const handleFlashlightButtonClick = () => {
+    setIsFlashlightVisible(!isFlashlightVisible);
+    setIsCounterVisible(false);
+    setIsFlashlightOn(!isFlashlightOn);
+  };
+
+  const handleCounterButtonClick = () => {
+    setIsCounterVisible(true);
+    setIsFlashlightVisible(false);
+  };
+
+  const handleMinusOneButtonClick = () => {
+    setCounterValue(counterValue - 1);
+  };
+
+  const handlePlusOneButtonClick = () => {
+    setCounterValue(counterValue + 1);
+  };
+
+  const handleBackButtonClick = () => {
+    setIsFlashlightVisible(false);
+    setIsCounterVisible(false);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      {isFlashlightVisible ? (
+        <>
+          <Ionicons
+            name={isFlashlightOn ? 'flash' : 'flash-off'}
+            size={100}
+            color={isFlashlightOn ? 'yellow' : 'gray'}
+            style={{ marginBottom: 20 }}
+          />
+          <Button
+            title={isFlashlightOn ? 'Off' : 'On'}
+            onPress={() => {
+              setIsFlashlightOn(!isFlashlightOn);
+            }}
+          />
+          <Button title="Back" onPress={handleBackButtonClick} />
+        </>
+      ) : (
+        <>
+          <Button title="Flashlight" onPress={handleFlashlightButtonClick} />
+          {isCounterVisible && (
+            <>
+              <Text style={{ fontSize: 50 }}>{counterValue}</Text>
+              <View style={{ flexDirection: 'row', marginTop: 10 }}>
+                <Button title="-1" onPress={handleMinusOneButtonClick} />
+                <Button title="+1" onPress={handlePlusOneButtonClick} />
+              </View>
+              <Button title="Back" onPress={handleBackButtonClick} />
+            </>
+          )}
+          {!isCounterVisible && (
+            <Button title="Counter" onPress={handleCounterButtonClick} />
+          )}
+        </>
+      )}
       <StatusBar style="auto" />
     </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
